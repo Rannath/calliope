8af0bda08a3d494668992ac7bc8c3f360f2e0d86
Rannath <rannath@gmail.com>
2023-04-11 18:01:58 -0400

    hopefullfixes linux

--- tools/run.py
+++ tools/run.py
@@ -7,4 +7,4 @@ if globals.IsWindows():
     subprocess.call(["cmd.exe", "/c", "{}\\run.bat".format(globals.TOOLS_DIR), config, globals.PROJECT_NAME], cwd=os.getcwd())
 
 if globals.IsLinux():
-    subprocess.call(["cmd.exe", "/c", "{}\\run.bat".format(globals.TOOLS_DIR), config, globals.PROJECT_NAME], cwd=os.getcwd())
+    subprocess.call(["make"], cwd=os.getcwd())
