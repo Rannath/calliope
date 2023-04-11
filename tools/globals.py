f66ea8af2885e921735fde233c900d165914fa01
Rannath <rannath@gmail.com>
2023-04-11 18:17:16 -0400

    fixing toolchain

--- tools/globals.py
+++ tools/globals.py
@@ -2,6 +2,10 @@ VERSION_MAJOR = 0
 VERSION_MINOR = 0
 VERSION_REVISION = 2
 
+ENGINE_NAME = "calliope"
+PROJECT_NAME = "calliopeeditor"
+TOOLS_DIR = "tools"
+
 import sys, platform
 PLATFORM = sys.platform
 for x in platform.uname():
@@ -10,10 +14,25 @@ for x in platform.uname():
         break
 
 def IsWindows():
-    return PLATFORM == "windows"
+    return PLATFORM == "windows" or PLATFORM == "win32"
 
 def IsLinux():
     return PLATFORM == "linux"
 
 def IsMac():
     return PLATFORM == "darwin"
+
+def ProcessArguments(argv):
+    ret = {} # return a key:value dict
+    for arg in argv:
+        try:
+            k = arg[0:arg.index("=")]
+            v = arg[arg.index("=")+1:]
+        except:
+            k = arg
+            v = 0
+        ret[k] = v
+    return ret
+
+def GetArgumentValue(args, name, default):
+    return args[name] if name in args else default
--- tools/version.py
+++ tools/version.py
@@ -1,3 +1,3 @@
 import globals
 
-print("Calliope tools - v{}.{}.{}".format(globals.VERSION_MAJOR, globals.VERSION_MINOR, globals.VERSION_REVISION))
+print("Calliope tools - v{}.{}.{}".format(globals.VERSION_MAJOR, globals.VERSION_MINOR, globals.VERSION_REVISION))cli
